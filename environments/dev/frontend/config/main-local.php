2194f3d80fcc3bc099b25152e34b4f550d64c982
Albert Borsos <albertborsos@me.com>
2014-09-02 23:59:20 +0200

    gii class added

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -30,6 +30,7 @@ if (!YII_ENV_TEST) {
     $config['modules']['debug'] = 'yii\debug\Module';
 
     $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = ['class' => 'yii\gii\Module'];
 }
 
 return $config;
